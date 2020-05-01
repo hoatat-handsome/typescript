7d4f5815c5ad998430fa83b6e37bae77b41764af
hoatat-handsome <tquangh@gmail.com>
2020-05-01 11:07:31 +0700

    Modified lesson17

--- dist/lesson17.js
+++ dist/lesson17.js
@@ -1,14 +1,21 @@
 "use strict";
 var Person17 = /** @class */ (function () {
-    function Person17() {
+    function Person17(name) {
+        this.name = name;
+        console.log(this.name + " constructor");
     }
+    Person17.talk = function (name) {
+        console.log(name + " static of talk");
+    };
     Person17.prototype.run = function () {
         console.log("Run function is running...");
     };
     return Person17;
 }());
-var p1 = new Person17();
-console.log(p1.run === Person17.prototype.run);
+var p1 = new Person17('Hoa Tat');
+p1.run();
+//console.log(p1.run === Person17.prototype.run);
+Person17.talk("Hoa Tat");
 //console.log(typeof p1);
 //console.log(typeof Person);
 //# sourceMappingURL=lesson17.js.map
--- lesson17.ts
+++ lesson17.ts
@@ -1,10 +1,23 @@
 class Person17 {
+        
+    constructor(name: string) {
+        this.name = name;
+        console.log(this.name + " constructor");
+    }
+
+    static talk(name: string) {        
+        console.log(name + " static of talk");
+    }
+
     run() {
         console.log("Run function is running...");
     }
 }
 
-let p1 = new Person17();
-console.log(p1.run === Person17.prototype.run);
+let p1 = new Person17('Hoa Tat');
+p1.run();
+//console.log(p1.run === Person17.prototype.run);
+
+Person17.talk("Hoa Tat");
 //console.log(typeof p1);
 //console.log(typeof Person);
